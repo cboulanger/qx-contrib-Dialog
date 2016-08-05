e39fc6b62bc17baa2f9ff204dcd597f71d65d1f6
jbruwes <jbruwes@redaktr.com>
2016-08-05 13:38:52 +0300

    Beautify

--- source/class/dialog/FormRenderer.js
+++ source/class/dialog/FormRenderer.js
@@ -1,40 +1,19 @@
-/* ************************************************************************
-
-   qooxdoo dialog library
-  
-   http://qooxdoo.org/contrib/catalog/#Dialog
-  
-   Copyright:
-     2007-2014 Christian Boulanger
-  
-   License:
-     LGPL: http://www.gnu.org/licenses/lgpl.html
-     EPL: http://www.eclipse.org/org/documents/epl-v10.php
-     See the LICENSE file in the project's top-level directory for details.
-  
-   Authors:
-   *  Christian Boulanger (cboulanger)
-  
-************************************************************************ */
-/*global qx dialog*/
-
 /**
+ * 
  * Form renderer renderer for {@link qx.ui.form.Form}. This is a 
  * single row renderer adapted for the dialog.Form widget. Main
  * difference is that the form allows text-only labels without a corresponding
  * form element which can serve, for example, for explanatory text.
+ * 
  */
-qx.Class.define("dialog.FormRenderer", 
-{
-  extend : qx.ui.form.renderer.Single,
-  implement : qx.ui.form.renderer.IFormRenderer,
-
-  members :
-  {
-    _row : 0,
-    _buttonRow : null,
-    
+qx.Class.define("dialog.FormRenderer", {
+  extend: qx.ui.form.renderer.Single,
+  implement: qx.ui.form.renderer.IFormRenderer,
+  members: {
+    _row: 0,
+    _buttonRow: null,
     /**
+     * 
      * Add a group of form items with the corresponding names. The names are
      * displayed as label.
      * The title is optional and is used as grouping for the given form 
@@ -43,113 +22,78 @@ qx.Class.define("dialog.FormRenderer",
      * @param items {qx.ui.core.Widget[]} An array of form items to render.
      * @param names {String[]} An array of names for the form items.
      * @param title {String?} A title of the group you are adding.
+     * 
      */
-    addItems : function(items, names, title) {
-      /*
-       * add the header
-       */
+    addItems: function(items, names, title) {
       if (title != null) {
         this._add(
-          this._createHeader(title), {row: this._row, column: 0, colSpan: 2}
+          this._createHeader(title), {
+            row: this._row,
+            column: 0,
+            colSpan: 2
+          }
         );
         this._row++;
       }
-      
-      /*
-       * add the items
-       */ 
-      for (var i = 0; i < items.length; i++) 
-      {
-        /*
-         * current item
-         */
+      for (var i = 0; i < items.length; i++) {
         var item = items[i];
-
-        /*
-         * radio group
-         */
-        if ( item instanceof qx.ui.form.RadioGroup )
-        {
-          /*
-           * create horizontal radio group for a small
-           * number of radio buttons 
-           */
-          if ( item.getUserData("orientation") == "horizontal" )
-          {
-            var widget = this._createHBoxForRadioGroup( item );
+        if (item instanceof qx.ui.form.RadioGroup) {
+          if (item.getUserData("orientation") == "horizontal") {
+            var widget = this._createHBoxForRadioGroup(item);
+          } else {
+            var widget = this._createWidgetForRadioGroup(item);
           }
-          else
-          {
-            var widget = this._createWidgetForRadioGroup( item );
-          }       
-        }
-        
-        /*
-         * other form widgets
-         */
-        else
-        {
+        } else {
           var widget = item;
         }
-        
-        /*
-         * Excluded form elements, used for full-width
-         * labels. this should be implemented differently,
-         * though
-         */
-        if ( names[i] && item.getUserData("excluded") )
-        {
-          var label = new qx.ui.basic.Label( names[i] );
+        if (names[i] && item.getUserData("excluded")) {
+          var label = new qx.ui.basic.Label(names[i]);
           label.setRich(true);
-          this._add( label,  {row: this._row, column: 0, colSpan : 2 } );
+          this._add(label, {
+            row: this._row,
+            column: 0,
+            colSpan: 2
+          });
+        } else if (item instanceof qx.ui.form.CheckBox) {
+          this._add(widget, {
+            row: this._row,
+            column: 0,
+            colSpan: 2
+          });
+          this._getLayout().getCellWidget(this._row, 0).setAlignX("left");
+        } else if (!names[i]) {
+          this._add(widget, {
+            row: this._row,
+            column: 0,
+            colSpan: 2
+          });
+        } else {
+          var label = this._createLabel(names[i], item);
+          label.setRich(true);
+          this._add(label, {
+            row: this._row,
+            column: 0
+          });
+          this._add(widget, {
+            row: this._row,
+            column: 1
+          });
         }
-
-        /**
-         * If CheckBox, assign the whole width to the widget.
-         */
-        else if ( item instanceof qx.ui.form.CheckBox )
-        {
-            this._add( widget, {row: this._row, column:0, colSpan: 2 } );
-            this._getLayout().getCellWidget(this._row,0).setAlignX("left");
-        }
-        
-        /*
-         * if the label is null, use the full width for the widget
-         * doesn't work yet
-         */
-        else if ( ! names[i]  )
-        {
-          this._add( widget, {row: this._row, column: 0, colSpan : 2 } );
-        }
-        
-        /*
-         * normal case: label in col 0, form element in col 2
-         */
-        else
-        {
-          var label = this._createLabel( names[i], item);
-          label.setRich(true);          
-          this._add( label, {row: this._row, column: 0} );
-          this._add( widget, {row: this._row, column: 1} );
-        }
-        
-        /*
-         * increment row
-         */
-         this._row++;
-        
+        this._row++;
       }
     },
-
     /**
+     * 
      * Takes the items of the given RadioGroup and adds the to a Composite. 
      * 
      * @param group {qx.ui.form.RadioGroup} The RadioGroup which needs to be 
      *   added.
+     * 
      * @return {qx.ui.container.Composite} A composite containing the items of 
      *   the RadioGroup.
+     * 
      */
-    _createWidgetForRadioGroup : function(group) {
+    _createWidgetForRadioGroup: function(group) {
       var widget = new qx.ui.container.Composite(new qx.ui.layout.VBox(5));
       var items = group.getItems();
       for (var i = 0; i < items.length; i++) {
@@ -157,8 +101,8 @@ qx.Class.define("dialog.FormRenderer",
       }
       return widget;
     },
-    
     /**
+     * 
      * Takes the items of the given RadioGroup and adds the to a Composite. 
      * The composite has a HBox layout so the RadioButtons will be alligned
      * horizontally. This is only useful for a small number of RadioButtons
@@ -166,10 +110,12 @@ qx.Class.define("dialog.FormRenderer",
      * 
      * @param group {qx.ui.form.RadioGroup} The RadioGroup which needs to be 
      *   added.
+     * 
      * @return {qx.ui.container.Composite} A composite containing the items of 
      *   the RadioGroup.
+     * 
      */
-    _createHBoxForRadioGroup : function(group) {
+    _createHBoxForRadioGroup: function(group) {
       var widget = new qx.ui.container.Composite(new qx.ui.layout.HBox(5));
       var items = group.getItems();
       for (var i = 0; i < items.length; i++) {
@@ -177,7 +123,5 @@ qx.Class.define("dialog.FormRenderer",
       }
       return widget;
     }
-    
-    
   }
 });
--- source/class/dialog/Login.js
+++ source/class/dialog/Login.js
@@ -1,24 +1,5 @@
-/* ************************************************************************
-
-   qooxdoo dialog library
-
-   http://qooxdoo.org/contrib/catalog/#Dialog
-
-   Copyright:
-     2007-2014 Christian Boulanger
-
-   License:
-     LGPL: http://www.gnu.org/licenses/lgpl.html
-     EPL: http://www.eclipse.org/org/documents/epl-v10.php
-     See the LICENSE file in the project's top-level directory for details.
-
-   Authors:
-   *  Christian Boulanger (cboulanger)
-
-************************************************************************ */
-/*global qx dialog*/
-
 /**
+ * 
  * A dialog for authentication and login. 
  * 
  * Please note that (since 0.6) the API has changed:
@@ -29,44 +10,37 @@
  * first argument (the error message/object) if authentication has FAILED or with
  * a falsy value (null/undefined) as first argument, plus with a second optional 
  * argument (that can contain user information) if it was SUCCESSFUL. The 
- * authenticating function must now be stored in the checkCredentials property.  
+ * authenticating function must now be stored in the checkCredentials property.
+ *   
  */
-qx.Class.define("dialog.Login",
-{
-  extend : dialog.Dialog,
-
-  /*
-  *****************************************************************************
-     PROPERTIES
-  *****************************************************************************
-  */
-  properties :
-  {
-
+qx.Class.define("dialog.Login", {
+  extend: dialog.Dialog,
+  properties: {
     /**
+     * 
      * A html text that is displayed below the image (if present)
      * and above the login
+     * 
      */
-    text :
-    {
-      check : "String",
-      nullable : true,
-      apply : "_applyText"
+    text: {
+      check: "String",
+      nullable: true,
+      apply: "_applyText"
     },
-
     /**
+     * 
      * The name of the font in the theme that should be applied to
      * the text
+     * 
      */
-    textFont :
-    {
-      check    : "String",
-      nullable : true,
-      init     : "bold",
-      apply    : "_applyTextFont"
+    textFont: {
+      check: "String",
+      nullable: true,
+      init: "bold",
+      apply: "_applyTextFont"
     },
-
     /**
+     * 
      * An asyncronous function to check the given credentials.
      * The function signature is (username, password, callback).
      * In case the login fails, the callback must be called with a string
@@ -74,139 +48,95 @@ qx.Class.define("dialog.Login",
      * due to authentication itself. If the login succeeds, the argument
      * must be undefined or null. You can pass a second argument containing
      * user information.
+     * 
      */
-    checkCredentials :
-    {
-        check : "Function",
-        nullable : false
+    checkCredentials: {
+      check: "Function",
+      nullable: false
     },
-    
     /**
+     * 
      * Whether to show a button with "Forgot Password?"
+     * 
      */
-    showForgotPassword :
-    {
-      check : "Boolean",
-      nullable : false,
-      init : false,
-      event : "changeShowForgotPassword"
+    showForgotPassword: {
+      check: "Boolean",
+      nullable: false,
+      init: false,
+      event: "changeShowForgotPassword"
     },
-    
     /**
+     * 
      * The function that is called when the user clicks on the "Forgot Password?"
      * button
+     * 
      */
-    forgotPasswordHandler :
-    {
+    forgotPasswordHandler: {
       check: "Function"
     }
-    
   },
-
-  /*
-  *****************************************************************************
-     EVENTS
-  *****************************************************************************
-  */
-  events :
-  {
+  events: {
     /**
+     * 
      * Event dispatched when login was successful
+     * 
      */
-    "loginSuccess" : "qx.event.type.Data",
-
+    "loginSuccess": "qx.event.type.Data",
     /**
+     * 
      * Data event dispatched when login failed, event data
      * contains a reponse message
+     * 
      */
-    "loginFailure"  : "qx.event.type.Data"
+    "loginFailure": "qx.event.type.Data"
   },
-
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
-  members :
-  {
-    /*
-    ---------------------------------------------------------------------------
-       PRIVATE MEMBERS
-    ---------------------------------------------------------------------------
-    */
-    _text : null,
-    _username : null,
-    _password : null,
-    
-    /*
-    ---------------------------------------------------------------------------
-     APPLY AND PRIVATE METHODS
-    ---------------------------------------------------------------------------
-    */
-
+  members: {
+    _text: null,
+    _username: null,
+    _password: null,
     /**
-    * Apply function used by proterty {@link #text}
-    * @param value {String} New value
-    * @param old {String} Old value
-    */
-    _applyText : function( value, old )
-    {
-      this._text.setValue( value );
-      this._text.setVisibility( value ? "visible" : "excluded" );
-    },
-
-	/**
-	 * Apply function used by proterty {@link #textFont}
-	 * @param value {String} New value
-	 */
-    _applyTextFont : function( value )
-    {
-      this._text.setFont( value );
-    },
-
-    /*
-    ---------------------------------------------------------------------------
-       WIDGET LAYOUT
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * Create the main content of the widget
+     * 
+     * Apply function used by proterty {@link #text}
+     * 
+     * @param value {String} New value
+     * @param old {String} Old value
+     * 
      */
-    _createWidgetContent : function()
-    {
-
-      /*
-       * groupbox
-       */
+    _applyText: function(value, old) {
+      this._text.setValue(value);
+      this._text.setVisibility(value ? "visible" : "excluded");
+    },
+    /**
+     * 
+     * Apply function used by proterty {@link #textFont}
+     * 
+     * @param value {String} New value
+     * 
+     */
+    _applyTextFont: function(value) {
+      this._text.setFont(value);
+    },
+    /**
+     * 
+     * Create the main content of the widget
+     * 
+     */
+    _createWidgetContent: function() {
       var groupboxContainer = new qx.ui.groupbox.GroupBox().set({
         contentPadding: [16, 16, 16, 16]
       });
       var layout = new qx.ui.layout.VBox(10);
       layout.setAlignX("center");
-      groupboxContainer.setLayout( layout );
-      this.add( groupboxContainer );
-
-      /*
-       * add image
-       */
+      groupboxContainer.setLayout(layout);
+      this.add(groupboxContainer);
       this._image = new qx.ui.basic.Image();
       this._image.setVisibility("excluded");
-      groupboxContainer.add( this._image );
-
-      /*
-       * add text
-       */
+      groupboxContainer.add(this._image);
       this._text = new qx.ui.basic.Label();
       this._text.setAllowStretchX(true);
       this._text.setVisibility("excluded");
       this.setTextFont("bold");
-
-      groupboxContainer.add( this._text );
-
-      /*
-       * grid layout with login fields
-       */
+      groupboxContainer.add(this._text);
       var gridContainer = new qx.ui.container.Composite;
       var gridLayout = new qx.ui.layout.Grid(9, 5);
       gridLayout.setColumnAlign(0, "right", "top");
@@ -217,112 +147,91 @@ qx.Class.define("dialog.Login",
       gridContainer.setAlignX("center");
       gridContainer.setMinWidth(200);
       gridContainer.setMaxWidth(400);
-      groupboxContainer.add( gridContainer );
-
-      /*
-       * Labels
-       */
-      var labels = [this.tr("Name"), this.tr("Password") ];
-      for (var i=0; i<labels.length; i++) {
+      groupboxContainer.add(gridContainer);
+      var labels = [this.tr("Name"), this.tr("Password")];
+      for (var i = 0; i < labels.length; i++) {
         gridContainer.add(new qx.ui.basic.Label(labels[i]).set({
           allowShrinkX: false,
           paddingTop: 3
-        }), {row: i, column : 0});
+        }), {
+          row: i,
+          column: 0
+        });
       }
-
-      /*
-       * Text fields
-       */
       this._username = new qx.ui.form.TextField();
       this._password = new qx.ui.form.PasswordField();
-
-      this._password.addListener("keypress",function(e){
-        if ( e.getKeyIdentifier() == "Enter" )
-        {
+      this._password.addListener("keypress", function(e) {
+        if (e.getKeyIdentifier() == "Enter") {
           this._callCheckCredentials();
         }
-      },this);
-
+      }, this);
       gridContainer.add(this._username.set({
         allowShrinkX: false,
         paddingTop: 3
-      }), {row: 0, column : 1});
-
-      gridContainer.add(this._password .set({
+      }), {
+        row: 0,
+        column: 1
+      });
+      gridContainer.add(this._password.set({
         allowShrinkX: false,
         paddingTop: 3
-      }), {row: 1, column : 1});
-
-      /*
-       * Add message label
-       */
+      }), {
+        row: 1,
+        column: 1
+      });
       this._message = new qx.ui.basic.Label();
       this._message.setRich(true);
       this._message.setAllowStretchX(true);
       this._message.setVisibility("excluded");
-      groupboxContainer.add( this._message );
-
-      /*
-       * Login Button
-       */
-      var loginButton = this._loginButton =  new qx.ui.form.Button(this.tr("Login"));
+      groupboxContainer.add(this._message);
+      var loginButton = this._loginButton = new qx.ui.form.Button(this.tr("Login"));
       loginButton.setAllowStretchX(false);
       loginButton.addListener("execute", this._callCheckCredentials, this);
-
-      /*
-       * Cancel Button
-       */
       var cancelButton = this._createCancelButton();
-      
-      /*
-       * Forgot Password Button
-       */
       var forgotPasswordButton = new qx.ui.form.Button(this.tr("Forgot Password?"));
-      forgotPasswordButton.addListener("click",function(){
+      forgotPasswordButton.addListener("click", function() {
         this.getForgotPasswordHandler()();
-      },this);
+      }, this);
       this.bind("showForgotPassword", forgotPasswordButton, "visibility", {
-        converter : function(v){ return v ? "visible" : "excluded" }
+        converter: function(v) {
+          return v ? "visible" : "excluded"
+        }
       });
-
-      /*
-       * buttons pane
-       */
       var buttonPane = new qx.ui.container.Composite();
       buttonPane.setLayout(new qx.ui.layout.HBox(5));
       buttonPane.add(loginButton);
       buttonPane.add(cancelButton);
       buttonPane.add(forgotPasswordButton);
-      gridContainer.add(
-         buttonPane,{ row : 3, column : 1}
-      );
-
+      gridContainer.add(buttonPane, {
+        row: 3,
+        column: 1
+      });
     },
-
     /**
+     * 
      * Calls the checkCredentials callback function with username, password and
      * the final callback, bound to the context object.
+     * 
      */
-    _callCheckCredentials : function()
-    {
+    _callCheckCredentials: function() {
       this.getCheckCredentials()(
         this._username.getValue(),
         this._password.getValue(),
-        typeof Function.prototype.bind === "function" ? 
-          this._handleCheckCredentials.bind(this) :
-          qx.lang.Function.bind( this._handleCheckCredentials, this )
+        typeof Function.prototype.bind === "function" ?
+        this._handleCheckCredentials.bind(this) :
+        qx.lang.Function.bind(this._handleCheckCredentials, this)
       );
     },
-
     /**
+     * 
      * Handle click on cancel button
+     * 
      */
-    _handleCancel : function()
-    {
+    _handleCancel: function() {
       this.hide();
     },
-
     /**
+     * 
      * Handler function called from the function that checks the credentials 
      * with the result of the authentication process.
      * 
@@ -332,46 +241,27 @@ qx.Class.define("dialog.Login",
      * the callback function in the callback property is called with null (success)
      * or the error value.
      * @param data {unknown|undefined} Optional second argument wich can contain user information
+     * 
      */
-    _handleCheckCredentials : function( err, data )
-    {
-      /*
-       * clear password field and message label
-       */
+    _handleCheckCredentials: function(err, data) {
       this._password.setValue("");
       this.setMessage(null);
-
-      /*
-       * check result
-       */
-      if ( err )
-      {
-        this.fireDataEvent("loginFailure", err );
-      }
-      else
-      {
+      if (err) {
+        this.fireDataEvent("loginFailure", err);
+      } else {
         this.fireDataEvent("loginSuccess", data);
-        this.hide();        
+        this.hide();
       }
-      
-      // Call final callback
-      if( this.getCallback() )
-      {
+      if (this.getCallback()) {
         this.getCallback()(err, data);
       }
     },
-
-    /*
-    ---------------------------------------------------------------------------
-      API METHODS
-    ---------------------------------------------------------------------------
-    */
-
     /**
-    * @override
-    */
-    hide : function()
-    {
+     * 
+     * @override
+     * 
+     */
+    hide: function() {
       this._password.setValue("");
       this.setMessage(null);
       this.base(arguments);
